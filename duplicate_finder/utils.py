7ca0a0961fa4aeed34ddb0ffab0596ee8a5d623b
Louis MARTIN <louisrtm@gmail.com>
2017-11-27 16:03:16 +0100

    Refactor find_duplicates

--- duplicate_finder/utils.py
+++ duplicate_finder/utils.py
@@ -19,20 +19,20 @@ def create_tree(root_path):
         items = [(name, False) for name in dirnames] + \
                 [(name, True) for name in filenames]
         for name, is_file in items:
-            node = HashNode(name, parent=parent_node, is_file=is_file)
+            node = HashNode(name, parent=parent_node)
             path = os.path.join(dirpath, name)
             nodes_by_path[path] = node
     return nodes_by_path
 
 
-def group_nodes_by_hash(root_nodes, nodes_by_path,
+def group_nodes_by_hash(root_paths, nodes_by_path,
                         skip_duplicates_children=False):
     print('Computing hashes and regrouping nodes...')
     start = time.time()
     nodes_by_hash = defaultdict(list)
 
     # Traverse the tree
-    nodes_to_explore = root_nodes
+    nodes_to_explore = [nodes_by_path[path] for path in root_paths]
     while len(nodes_to_explore) > 0:
         node = nodes_to_explore.pop()
         if not (skip_duplicates_children and node.hash_md5 in nodes_by_hash):
@@ -44,41 +44,49 @@ def group_nodes_by_hash(root_nodes, nodes_by_path,
     return nodes_by_hash
 
 
-def find_duplicates(paths):
-    root_nodes = []
+def explore_paths(paths):
     nodes_by_path = {}
     print('Discovering files...')
     for path in paths:
         # Create tree
         print('\t{}'.format(path))
         assert os.path.exists(path), '{} does not exist'.format(path)
-        current_nodes_by_path = create_tree(path)
-        root_nodes.append(current_nodes_by_path[path])
-        nodes_by_path.update(current_nodes_by_path)
+        nodes_by_path.update(create_tree(path))
+    return nodes_by_path
+
+
+def size_to_str(size):
+    return '{:0.2f} MB'.format(size/1e6)
+
+
+def find_matching_nodes(pattern, nodes_by_path):
+    return [node for path, node in nodes_by_path.items()
+            if re.search(pattern, path, re.IGNORECASE)]
+
+
+def find_duplicates(root_paths):
+    nodes_by_path = explore_paths(root_paths)
 
     # Regroup nodes by hash
-    print(root_nodes)
-    nodes_by_hash = group_nodes_by_hash(root_nodes,
+    nodes_by_hash = group_nodes_by_hash(root_paths,
                                         nodes_by_path,
                                         skip_duplicates_children=True)
     duplicate_hashes = [k for k, v in nodes_by_hash.items() if len(v) > 1]
+
     # Compute duplicate sizes for each duplicate
     print('Getting duplicates sizes...')
     start = time.time()
-    sizes = []
+    duplicated_sizes = []
     for hash_val in duplicate_hashes:
         duplicate_nodes = nodes_by_hash[hash_val]
         nb_duplicates = len(duplicate_nodes) - 1
         duplicated_size = nb_duplicates * duplicate_nodes[0].storage_size
-        sizes.append(duplicated_size)
+        duplicated_sizes.append(duplicated_size)
     print('\t{:.2f}s'.format(time.time() - start))
 
-    # Sort and print duplicated nodes by size
     print('Sorting duplicates...')
-    start = time.time()
-    for idx in np.argsort(sizes)[::-1]:
-        size = sizes[idx]
+    # Sort and print duplicated nodes by size
+    for idx in np.argsort(duplicated_sizes)[::-1][:100]:
         nodes = nodes_by_hash[duplicate_hashes[idx]]
-        print('{:0.2f} MB'.format(size/1e6))
+        print(size_to_str(duplicated_sizes[idx]))
         print(''.join(['\t{}\n'.format(node.storage_path) for node in nodes]))
-    print('\t{:.2f}s'.format(time.time() - start))
