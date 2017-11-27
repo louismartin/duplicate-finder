432fc24210bda1e978717def0d7c117098814b51
Louis MARTIN <louisrtm@gmail.com>
2017-11-27 16:01:10 +0100

    Handle empty directories and symlinks

--- duplicate_finder/hashnode.py
+++ duplicate_finder/hashnode.py
@@ -23,9 +23,9 @@ def get_file_content_md5(file_path):
     return hash_function.digest()
 
 
-def get_file_name_md5(file_path):
+def get_base_name_md5(path):
     hash_function = hashlib.md5()
-    hash_function.update(os.path.basename(file_path).encode('utf8'))
+    hash_function.update(os.path.basename(path).encode('utf8'))
     return hash_function.digest()
 
 
@@ -54,7 +54,7 @@ def compute_file_hash(file_path,
     if hash_file_content:
         hash_function.update(get_file_content_md5(file_path))
     if hash_file_name:
-        hash_function.update(get_file_name_md5(file_path))
+        hash_function.update(get_base_name_md5(file_path))
     if hash_file_size:
         hash_function.update(get_file_size_md5(file_path))
     return hash_function.digest()
@@ -63,23 +63,6 @@ def compute_file_hash(file_path,
 class HashNode(Node):
     hash_file_content = False
     hash_file_name = True
-    hash_file_size = True
-
-    @lazyproperty
-    def hash_md5(self):
-        '''
-        Recursive lazy property to compute the md5 hash based on the hash of
-        all children.
-        '''
-        if self.is_file:
-            return compute_file_hash(self.storage_path,
-                                     self.hash_file_content,
-                                     self.hash_file_name,
-                                     self.hash_file_size)
-        hash_function = hashlib.md5()
-        for child in sorted(self.children, key=lambda c: c.hash_md5):
-            hash_function.update(child.hash_md5)
-        return hash_function.digest()
 
     @lazyproperty
     def storage_path(self):
@@ -91,3 +74,39 @@ class HashNode(Node):
         if self.is_file:
             return get_file_size(self.storage_path)
         return sum([child.storage_size for child in self.children])
+    hash_file_size = True
+
+    @lazyproperty
+    def is_file(self):
+        return os.path.isfile(self.storage_path)
+
+    @lazyproperty
+    def is_link(self):
+        return os.path.islink(self.storage_path)
+
+    @lazyproperty
+    def hash_md5(self):
+        '''
+        Recursive lazy property to compute the md5 hash based on the hash of
+        all children.
+        '''
+        if self.is_leaf:
+            if self.is_file:
+                 return compute_file_hash(self.storage_path,
+                                          self.hash_file_content,
+                                          self.hash_file_name,
+                                          self.hash_file_size)
+            elif self.is_link:
+                 return get_base_name_md5(self.storage_path)
+            else:
+                 # Empty directory we only hash its name
+                 assert len(os.listdir(self.storage_path)) == 0, \
+                        'Non empty directory is a leaf: {}'.format(self.storage_path)
+                 return get_base_name_md5(self.storage_path)
+        hash_function = hashlib.md5()
+        for child in sorted(self.children, key=lambda c: c.hash_md5):
+            hash_function.update(child.hash_md5)
+        return hash_function.digest()
+
+    def is_in_dir(self, dir_path):
+        return self.storage_path.startswith(dir_path)
