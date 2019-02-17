a94032645cfa2d6b96cac3aa1c62cc82d1e672bd
Louis Martin <louismartin@fb.com>
2019-02-17 21:28:55 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+setup(
+    name='duplicate-finder',
+    version='1.0',
+    description='Simple python tool to find exact file and folder duplicates on your computer and drives.',
+    author='Louis Martin',
+    author_email='louismartincs@gmail.com',
+    packages=['duplicate_finder'],
+)
